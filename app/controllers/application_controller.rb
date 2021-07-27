e768db126ed6b6ef74a9e03b5e83b93c8b3bff40
emiko <31835314+siroemk@users.noreply.github.com>
2021-07-27 22:58:28 +0900

    set_localeメソッドをApplicationControllerに移動した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@
 class ApplicationController < ActionController::Base
   private
 
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
   def default_url_options(options = {})
     { locale: I18n.locale }.merge options
   end
