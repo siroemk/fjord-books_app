b94062bf50df35afedc4aecd4425b77cae5dc47e
emiko <31835314+siroemk@users.noreply.github.com>
2021-08-18 12:59:07 +0900

    ユーザ一覧とユーザ詳細画面を作成した

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   root 'books#index'
   resources :books
+  resources :users
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
