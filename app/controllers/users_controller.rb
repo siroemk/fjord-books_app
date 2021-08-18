765f18d72e55a6705e886420af61ade57d70ca29
emiko <31835314+siroemk@users.noreply.github.com>
2021-08-18 13:47:00 +0900

    kaminariのデフォルトの表示件数を変更した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id).page(params[:page]).per(5)
+    @users = User.order(:id).page(params[:page])
   end
 
   def show
