444a7aff69c60b8ccb4e09f2347ecc9cd7a07bce
emiko <31835314+siroemk@users.noreply.github.com>
2021-09-06 13:10:04 +0900

    before_actionにfind_userを指定した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :find_user, only: %i[show followings followers]
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
 
   def show
-    @user = User.find(params[:id])
+    @user
   end
 
   def followings
