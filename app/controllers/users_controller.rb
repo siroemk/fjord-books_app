afeea1aeef1bfe5db9b5ecf1dc7258c1f2528226
emiko <31835314+siroemk@users.noreply.github.com>
2021-09-10 13:12:55 +0900

    メソッド名を修正した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :find_user, only: %i[show followings followers]
+  before_action :set_user, only: %i[show followings followers]
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 
   private
 
-  def find_user
+  def set_user
     @user = User.find(params[:id])
   end
 end
