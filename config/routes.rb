667f01c912cd1c9bfcb1297dccaf5321b7963389
emiko <31835314+siroemk@users.noreply.github.com>
2021-09-19 11:16:34 +0900

    usersコントローラの中身がCRUDアクションだけになるようにコントローラを分けた

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,8 @@ Rails.application.routes.draw do
   resources :books
   resources :users, only: %i(index show)
   resources :users do
-    member do
-      get 'followings'
-      get 'followers'
-    end
+    resources :followings, only: :index, module: 'users'
+    resources :followers, only: :index, module: 'users'
   end
   resources :relationships, only: %i[create destroy]
 end
