8b5707771b32f81881ad8e56eeb129db1158c314
emiko <31835314+siroemk@users.noreply.github.com>
2021-09-21 14:42:35 +0900

    コメント用のルーティングを追加した

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,18 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
   resources :users, only: %i[index show] do
     resource :relationships, only: %i[create destroy]
     scope module: :users do
       resources :followings, only: [:index]
       resources :followers, only: [:index]
     end
+  end
+  resources :books do
+    resources :comments, only: %i[create]
+  end
+
+  resources :reports do
+    resources :comments, only: %i[create]
   end
 end
