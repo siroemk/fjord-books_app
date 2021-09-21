d599cc2b221ef4c693899b0ff7c66a453dd7b72d
emiko <31835314+siroemk@users.noreply.github.com>
2021-09-21 11:20:22 +0900

    dependent: :destroyを付け直した

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
-  belongs_to :commentable, polymorphic: true, dependent: :destroy
+  belongs_to :commentable, polymorphic: true
 end
