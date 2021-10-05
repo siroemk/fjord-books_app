7270f2c2b3cd4f05d120d1f97dd1c17c8ed61833
emiko <31835314+siroemk@users.noreply.github.com>
2021-10-05 15:12:56 +0900

    commentsテーブルのuser_idカラムのnot null制約をはずす

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_043918) do
+ActiveRecord::Schema.define(version: 2021_10_05_042907) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2021_09_29_043918) do
     t.integer "commentable_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
   end
 
