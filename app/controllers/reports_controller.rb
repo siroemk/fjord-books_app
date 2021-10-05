1c5cc8616dc3ab7b9268d622da2722e42659f1b3
emiko <31835314+siroemk@users.noreply.github.com>
2021-10-05 23:30:41 +0900

    createのnewの行を書き直した

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -27,8 +27,7 @@ class ReportsController < ApplicationController
 
   # POST /reports or /reports.json
   def create
-    @report = Report.new(report_params)
-    @report.user = current_user
+    @report = current_user.report.new(report_params)
     if @report.save
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
