130950a3d55075e60ee1361b145bd325bd1c6693
emiko <31835314+siroemk@users.noreply.github.com>
2021-10-21 14:07:12 +0900

    Reportクラスのcreated_onメソッドのテストを書いた

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -11,4 +11,12 @@ class ReportTest < ActiveSupport::TestCase
     assert report.editable?(me)
     assert_not report.editable?(she)
   end
+
+  test '#created_on' do
+    me = User.create(email: 'me@example.com', password: 'password')
+    my_report = Report.create(user_id: me.id, title: '日報タイトル', content: '日報内容')
+
+    assert_not_equal Date, my_report.created_at.class
+    assert_equal Date, my_report.created_on.class
+  end
 end
