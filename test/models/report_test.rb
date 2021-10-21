800cd2a9a65f95d436d003584871bc3a05358b68
emiko <31835314+siroemk@users.noreply.github.com>
2021-10-21 16:54:08 +0900

    変数を修正した

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -6,10 +6,10 @@ class ReportTest < ActiveSupport::TestCase
   test '#editable?' do
     me = User.create(email: 'me@example.com', password: 'password')
     she = User.create(email: 'she@example.com', password: 'password')
-    report = Report.create(user_id: me.id, title: '日報タイトル', content: '日報内容')
+    my_report = Report.create(user_id: me.id, title: '日報タイトル', content: '日報内容')
 
-    assert report.editable?(me)
-    assert_not report.editable?(she)
+    assert my_report.editable?(me)
+    assert_not my_report.editable?(she)
   end
 
   test '#created_on' do
