bdc317dff52b9827232e7467eedc7a39863597e2
emiko <31835314+siroemk@users.noreply.github.com>
2021-10-21 12:51:17 +0900

    Reportクラスのeditable?メソッドのテストを書いた

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,12 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    me = User.create(email: 'me@example.com', password: 'password')
+    she = User.create(email: 'she@example.com', password: 'password')
+    report = Report.create(user_id: me.id, title: '日報タイトル', content: '日報内容')
+
+    assert report.editable?(me)
+    assert_not report.editable?(she)
+  end
 end
